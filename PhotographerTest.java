be1fbd01f538bcc5c027ca1a7af3ebe771ef2c58
Ewen Carr <ewencarr@hotmail.com>
2017-03-07 15:29:55 +0000

    can add to collection

--- PhotographerTest.java
+++ PhotographerTest.java
@@ -3,6 +3,7 @@ import org.junit.*;
 
 public class PhotographerTest{
   Photographer photographer;
+  Camera camera;
 
   @Before
   public void before() {
@@ -17,6 +18,12 @@ public class PhotographerTest{
   @Test
   public void collectionStartsEmpty(){
     assertEquals(0, photographer.cameraCount());
+  }
+
+  @Test
+  public void canAddToCollection(){
+    photographer.addCamera(camera);
+    assertEquals(1, photographer.cameraCount());
   }
 
 
